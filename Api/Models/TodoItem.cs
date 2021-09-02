badfecaea3307513982418f5e9b6718c7a8080c2
phamthao80s <phamthao80s@gmail.com>
2021-09-02 10:32:26 +0100

    Prevent over-posting
    Update TodoItems class to include secret field. Create a DTO model. Update the TodoItemsController to use TodoItemDTO to prevent secret field.

--- Api/Models/TodoItem.cs
+++ Api/Models/TodoItem.cs
@@ -6,5 +6,6 @@ namespace TodoApi.Models
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
+        public string Secret { get; set; }
     }
 }

1d64d2bbea41481d74d0dd8f0a30af0dc75fe667
Thao Pham <phamthao80s@gmail.com>
2022-01-06 14:30:50 +0000

    minimal hosting model

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,25 +1,21 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using TodoApi.Models;
 
-namespace TodoApi
-{
-#pragma warning disable CS1591
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            BuildWebHost(args).Run();
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
-#pragma warning restore CS1591
-}
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" }));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.Run();
